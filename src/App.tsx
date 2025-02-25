fc426f95b951af9d43c08f9bbafa8ae957dc92ba
Andreyhai <ustavitski.andrei@gmail.com>
2025-02-26 01:58:59 +0300

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
