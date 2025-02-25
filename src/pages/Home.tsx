6335c486d6dad236ab54ec487f471bbccd88c35e
Andreyhai <ustavitski.andrei@gmail.com>
2025-02-26 02:16:00 +0300

    Fix routing for GitHub Pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  return <div>
+    <span>Home Page</span>
+    <Link to="/about">About</Link>
+    </div>;
 };
 
 export default Home;
