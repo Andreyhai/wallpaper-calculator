f333888dc399462da6e8f0275c1dd86d76a079f9
Andreyhai <ustavitski.andrei@gmail.com>
2025-02-26 02:22:06 +0300

    Fix routing for GitHub Pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { HOME_ROUTE } from '../utils/PATHS';
 
 const About: React.FC = () => {
-  return <div>About Page</div>;
+    return <div>
+        <span>About Page</span>
+        <Link to={HOME_ROUTE}>Home</Link>
+    </div>;
 };
 
 export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ABOUT_ROUTE } from '../utils/PATHS';
 
 const Home: React.FC = () => {
-  return <div>
-    <span>Home Page</span>
-    <Link to="/about">About</Link>
+    return <div>
+        <span>Home Page</span>
+        <Link to={ABOUT_ROUTE}>About</Link>
     </div>;
 };
 
