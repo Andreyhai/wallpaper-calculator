0b8e1e7391b67bd7ea1ddeeabdb8e00168acef47
Andreyhai <ustavitski.andrei@gmail.com>
2025-02-26 00:52:47 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+
+import { ABOUT_ROUTE, HOME_ROUTE } from './utils/PATHS';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path={HOME_ROUTE} element={<Home />} />
+        <Route path={ABOUT_ROUTE} element={<About />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
