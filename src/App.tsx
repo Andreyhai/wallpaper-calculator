6cbcfe45f76e0572668776e4656ec2619005e1d7
Andreyhai <ustavitski.andrei@gmail.com>
2025-02-26 02:14:30 +0300

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 
-import { ABOUT_ROUTE } from './utils/PATHS';
+// import { ABOUT_ROUTE } from './utils/PATHS';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path={'/'} element={<Home />} />
-        <Route path={ABOUT_ROUTE} element={<About />} />
+        <Route path={'/about'} element={<About />} />
       </Routes>
     </Router>
   );
